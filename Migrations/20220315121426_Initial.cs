ddba13d407d40e41d08f0a659ee9a1bfd8e546ab
Chanjung Kim <freiyer.paxbun@gmail.com>
2022-03-15 21:15:05 +0900

    Add migration Initial

---
+++ Migrations/20220315121426_Initial.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AspNetCoreTodo.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Todos",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    CreationTime = table.Column<DateTimeOffset>(type: "TEXT", nullable: false),
+                    Body = table.Column<string>(type: "TEXT", nullable: false),
+                    Title = table.Column<string>(type: "TEXT", nullable: false),
+                    UpdateTime = table.Column<DateTimeOffset>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Todos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Comments",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    CreationTime = table.Column<DateTimeOffset>(type: "TEXT", nullable: false),
+                    Body = table.Column<string>(type: "TEXT", nullable: false),
+                    TodoId = table.Column<Guid>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Comments", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Comments_Todos_TodoId",
+                        column: x => x.TodoId,
+                        principalTable: "Todos",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_TodoId",
+                table: "Comments",
+                column: "TodoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Todos_CreationTime",
+                table: "Todos",
+                column: "CreationTime");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Todos_Title",
+                table: "Todos",
+                column: "Title");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Todos_UpdateTime",
+                table: "Todos",
+                column: "UpdateTime");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Comments");
+
+            migrationBuilder.DropTable(
+                name: "Todos");
+        }
+    }
+}
