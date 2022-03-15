3f03e09617fe90febdfb550f13d85c2c77666762
Chanjung Kim <freiyer.paxbun@gmail.com>
2022-03-15 18:57:30 +0900

    Add required services and middlewares in Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,32 @@
+using System.Reflection;
+using AspNetCoreTodo;
+
 var builder = WebApplication.CreateBuilder(args);
+var services = builder.Services;
+var environment = builder.Environment;
+
+services.AddSwaggerGen(options =>
+{
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename), true);
+    options.DescribeAllParametersInCamelCase();
+    options.SupportNonNullableReferenceTypes();
+    options.UseAllOfToExtendReferenceSchemas();
+    options.SchemaFilter<RequiredPropertiesSchemaFilter>();
+});
+services.AddRouting(options => { options.LowercaseUrls = true; });
+services.AddControllers();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
+app.UseHttpsRedirection();
+app.UseRouting();
+app.UseCors();
+app.MapControllers();
 app.Run();
